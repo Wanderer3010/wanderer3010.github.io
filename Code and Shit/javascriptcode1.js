f6484a2db67a8615b93b201556ce23612dbc837d
Wanderer3010 <126852681+Wanderer3010@users.noreply.github.com>
2023-03-03 17:14:06 +1100

    code lol

---
+++ Code and Shit/javascriptcode1.js
@@ -0,0 +1,9 @@
+let password = window.prompt('Do you wish to proceed?')
+if(password !='yes'){
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    
+}
+else{
+    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+
+};
