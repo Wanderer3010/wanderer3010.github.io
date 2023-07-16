5a71878dd51915e0b309ebc8f5493be17c1a33e9
Wanderer3010 <126852681+Wanderer3010@users.noreply.github.com>
2023-07-16 18:41:27 +1000

    Delete Code and Shit directory

--- Code and Shit/javascriptcode1.js
+++
@@ -1,9 +0,0 @@
-let password = window.prompt('Do you wish to proceed?')
-if(password !='yes'){
-    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-    
-}
-else{
-    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-
-};
